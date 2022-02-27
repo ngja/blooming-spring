37b0637d31def33601782a2a967e97ca3f7cea90
hansol <wkdqhrh00@naver.com>
2022-02-27 11:40:52 +0900

    Study Spring

---
+++ src/main/java/com/hansol/bloomingspring/BloomingSpringApplication.java
@@ -0,0 +1,13 @@
+package com.hansol.bloomingspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloomingSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BloomingSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hansol/bloomingspring/BloomingSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hansol.bloomingspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloomingSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
